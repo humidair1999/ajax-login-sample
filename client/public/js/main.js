182add6af4529222129f175fd8a9ae9e20b65777
J. Ky Marsh <jkymarsh@gmail.com>
2015-01-21 23:37:53 -0800

    add serialization of form input

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,1 +1,12 @@
+$('#user-login-form').on('submit', function(evt) {
+    var serializedFormInput = $(this).serializeArray(),
+        formData = {};
 
+    evt.preventDefault();
+
+    for (var i in serializedFormInput) {
+        formData[serializedFormInput[i].name] = serializedFormInput[i].value;
+    }
+
+    console.log(formData);
+});
