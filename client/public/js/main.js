08e2a57f90ac9ffff9a6885c9632ba68b9da81cd
J. Ky Marsh <jkymarsh@gmail.com>
2015-01-22 00:31:13 -0800

    show login form after seed has been retrieved

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,25 +1,33 @@
 (function() {
+    var activateLoginForm = function() {
+        $('#user-login-form-container').css('visibility', 'visible');
+
+        $('#user-login-form').on('submit', function(evt) {
+            var serializedFormInput = $(this).serializeArray(),
+                formData = {};
+
+            evt.preventDefault();
+
+            for (var i in serializedFormInput) {
+                formData[serializedFormInput[i].name] = serializedFormInput[i].value;
+            }
+
+            console.log(formData);
+        });
+    };
+    
     $.ajax({
         url: 'http://localhost:3000/api/v1/sessions/seed',
         type: 'get'
     }).done(function(data) {
         console.log(data);
 
-        $('#unique-seed').val(data.seed);
+        setTimeout(function() {
+            activateLoginForm();
+
+            $('#unique-seed').val(data.seed);
+        }, 500);
     }).fail(function() {
         console.log('error');
-    });
-
-    $('#user-login-form').on('submit', function(evt) {
-        var serializedFormInput = $(this).serializeArray(),
-            formData = {};
-
-        evt.preventDefault();
-
-        for (var i in serializedFormInput) {
-            formData[serializedFormInput[i].name] = serializedFormInput[i].value;
-        }
-
-        console.log(formData);
     });
 })();
